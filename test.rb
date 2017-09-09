1f7bfa42d3ded36a09fc555f71a1294f20cac58f
Jesus Garcia <jesusdaniel1901@gmail.com>
2017-09-09 11:41:20 -0300

    [refact]: using methods user_exist? and check_password?

--- test.rb
+++ test.rb
@@ -17,14 +17,15 @@ class Login
   end
 
   # Checks if user exists
-  def user_exists(user)
+  def user_exists?(user)
     @users.key?(user)
   end
 
   # Register user
   def register_user(user, password)
+    return false if user_exists?(user)
     @users[user] = password
-    login(user,password)
+    login(user,password) and return true
   end
 
   def remove_user(user)
@@ -32,18 +33,18 @@ class Login
     @users.delete(user)
   end
 
-  def check_password(user, password)
+  def check_password?(user, password)
     @users[user] == password
   end
 
   def update_password(user, old_password, new_password)
-    return false if !@users.key?(user) || @users[user] != old_password
+    return false if user_exists?(user) || !check_password?(user,old_password)
     @users[user] = new_password and return true
   end
 
   def login(user, password)
-    return false unless @users.key?(user)
-    sessions.push(user) and return true if @users[user] == password
+    return false unless user_exists?(user)
+    sessions.push(user) and return true if check_password?(user,password)
   end
 end
 
@@ -58,34 +59,5 @@ registered_users = {
 login = Login.new(registered_users)
 
 login.register_user('user4', 'pass4');
-
-# puts "CHECK PASSWORD #{login.check_password('user4','pass4')}"
-# puts "USER EXIST #{login.user_exists('user4')}"
-puts "REMOVE USER #{login.remove_user('user2')}"
-
-
 login.update_password('user3', 'pass3', 'pass5');
 login.login('user3', 'pass5');
-puts "LOGIN #{login.login('user4', 'pass4')} #{login.inspect}"
-# login.logout('user4');
-# puts "LOGOUT #{login.logout('user3')}"
-# puts "session #{login.sessions.inspect}"
-
-
-# puts("Registered Users: #{login.users}")
-# puts("Registered Passwords: #{login.passwords}")
-# puts("User -user1- exists? #{login.user_exists('user1222')}")
-# puts("User -user7- exists? #{login.user_exists('user7')}")
-# puts("Register -user9- pass: -pass9- #{login.register_user('user9', 'pass9')}")
-# puts("LOGIN: #{login.inspect}")
-# puts("User -user9- exists? #{login.user_exists('user9')}")
-# puts("Change Password -user9- to -pass1000- #{login.update_password('user9', 'pass9', 'pass1000')}")
-# puts("LOGIN: #{login.inspect}")
-# puts("Login -user9- #{login.login('user9', 'pass1000')}")
-# puts("LOGIN: #{login.inspect}")
-# puts("Logout -user9- #{login.logout('user9')}")
-# puts("LOGIN: #{login.inspect}")
-# puts("Check password -user9- correct #{login.check_password('user9', 'pass1000')}")
-# puts("Check password -user9- incorrect #{login.check_password('user9', 'pass9')}")
-# puts("Remove User -user9- #{login.remove_user('user9')}")
-# puts("LOGIN: #{login.inspect}")
